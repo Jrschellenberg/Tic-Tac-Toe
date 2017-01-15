24b5a8e0afb1dc3686c0d2e0182f05b024bdbacd
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-01-15 00:21:26 -0600

    Finished

--- js/app.js
+++ js/app.js
@@ -1,192 +1,204 @@
 /**
  * Created by Justin on 1/10/2017.
  */
+
+/*
+ Wrapping all the code in a self-invoking function literal.
+ */
 !function () {
 	'use strict';
 
+	//Instantiating my 'global' variables to be used throughout the program.
 	var TYPE = {
 		EMPTY: "E",
 		NOUGHT: "O",
 		CROSS: "X"
-	};
+	},
+	Cell,
+	Board;
 
-	function Cell(element) {
 
-		this.content = TYPE.EMPTY;
-		this.element = element;
-		//Cell.clear();
-	}
+	//Code for the Cell Class.
+	(function () {
+		Cell = function Cell(element) {
+
+			this.content = TYPE.EMPTY;
+			this.element = element;
+			//Cell.clear();
+		};
+
+		/*
+		 a method to clear the cells content and remove the styles associated with that content.
+		 */
+		Cell.prototype.clear = function () {
+			this.content = TYPE.EMPTY;
+			$(this.cell).removeClass("box-filled-1 box-filled-2");
+		};
+
+		/*
+		 The method used to give the appropriate class to the cell to "paint it" with the css styles
+		 content: the content being passed into the cell from the board object.
+		 */
+		Cell.prototype.paint = function (content) {
+			this.content = content;
+
+			if (this.content === TYPE.NOUGHT) {
+				$(this.element).addClass("box-filled-1");
+			}
+			else if (this.content === TYPE.CROSS) {
+				$(this.element).addClass("box-filled-2");
+			}
+			else {
+				throw new Error("The program ran into an error while painting the cell");
+			}
+		};
+
+	}());
 
 	/*
-	 a method to clear the cells content and remove the styles associated with that content.
+	Code for the Board Class.
+	Using Singleton Design pattern to make sure only 1 board is instantiated.
 	 */
-	Cell.prototype.clear = function () {
-		this.content = TYPE.EMPTY;
-		$(this.cell).removeClass("box-filled-1 box-filled-2");
-	};
+	(function () {
+		var instance;
 
-	/*
-	 The method used to give the appropriate class to the cell to "paint it" with the css styles
-	 content: the content being passed into the cell from the board object.
-	 */
-	Cell.prototype.paint = function (content) {
-		this.content = content;
+		Board = function Board() {
 
-		if (this.content === TYPE.NOUGHT) {
-			$(this.element).addClass("box-filled-1");
-		}
-		else if (this.content === TYPE.CROSS) {
-			$(this.element).addClass("box-filled-2");
-		}
-		else {
-			throw new Error("The program ran into an error while painting the cell");
-		}
-	};
-
-
-
-	/*
-	 The constructor of the Board object.
-	 */
-	function Board() {
-		this.result = "stillRunning";
-		this.turn = TYPE.NOUGHT;
-		this.displayTurn();
-		//initializing an empty array
-		this.items = $(".box");
-		this.cells = new Array();
-
-
-		this.board = [];
-
-
-		console.log(this.items);
-		for (var i = 0; i < this.items.length; i++) {
-			console.log(this.items[i]);
-			this.board[i] = i;
-			this.cells.push(new Cell(this.items[i]));
-
-		}
-		console.log(this.cells);
-	}
-
-	Board.prototype.paintCell = function (value) {
-		//console.log("got in here?");
-		//console.log(value);
-		//console.log(this.turn);
-		var played = false;
-		for (var i = 0; i < this.cells.length; i++) {
-			//Determines that cell being clicked on is an empty cell and matches the cell to the element selector.
-			if (value == this.cells[i].element && this.cells[i].content == "E") {
-				this.cells[i].paint(this.turn);
-				played = true;
+			if (instance) {
+				return instance;
 			}
-		}
-		console.log(played);
-		return played;
+			instance = this;
 
-	};
-	Board.prototype.isEmptyCell = function (value) {
-		for (var i = 0; i < this.cells.length; i++) {
+			this.result = "stillRunning";
+			this.turn = TYPE.NOUGHT;
+			this.displayTurn();
+			//initializing an  array of all the list items with class box.
+			this.items = $(".box");
+			this.cells = new Array();
+			this.board = [];
 
-			if (value === this.cells[i].element && this.cells[i].content === "E") {
-				return true;
+			for (var i = 0; i < this.items.length; i++) {
+				this.board[i] = i;
+				this.cells.push(new Cell(this.items[i]));
+
 			}
-		}
-		return false;
-	};
+			return instance;
+		};
 
-	Board.prototype.advanceTurn = function () {
-		if (this.turn === TYPE.NOUGHT) {
-			this.turn = "X";
-		}
-		else {
-			this.turn = "O";
-		}
-	};
-	Board.prototype.displayTurn = function () {
-		if (this.turn == "O") {
-			$("#player2").removeClass('active');
-			$("#player1").addClass('active');
-		}
-		else if (this.turn == "X") {
-			$("#player1").removeClass('active');
-			$("#player2").addClass('active');
-		}
-	};
-	/*
-	 This method is used to empty all of the cells on the board.
-	 */
-	Board.prototype.emptyAllCells = function () {
-		for (var i = 0; i < this.cells.length; i++) {
-			this.cells[i].clear();
-			$(this.items[i]).removeClass("box-filled-1 box-filled-2");
-		}
-	};
-	/*
-	 This method used to determine the amount of empty cells left.
-	 */
-	Board.prototype.emptyCellsLeftOnBoard = function () {
-		var indxs = [];
-		for (var itr = 0; itr < 9; itr++) {
-			if (this.cells[itr].content === "E") {
-				indxs.push(itr);
+		Board.prototype.paintCell = function (value) {
+			var played = false;
+			for (var i = 0; i < this.cells.length; i++) {
+				//Determines that cell being clicked on is an empty cell and matches the cell to the element selector.
+				if (value === this.cells[i].element && this.cells[i].content === TYPE.EMPTY) {
+					this.cells[i].paint(this.turn);
+					played = true;
+				}
 			}
-		}
-		return indxs;
-	};
+			return played;
 
+		};
+		Board.prototype.isEmptyCell = function (value) {
+			for (var i = 0; i < this.cells.length; i++) {
 
-	/*
-	 This method is used to determine if the game is finished, who won, and if there is a draw or not.
-	 */
-	Board.prototype.isTerminal = function () {
-		var board = this.cells;
-		//check rows
-		for (var i = 0; i <= 6; i = i + 3) {
-			if (board[i].content !== "E" && board[i].content === board[i + 1].content && board[i + 1].content == board[i + 2].content) {
-				this.result = board[i].content + "-won"; //update the state result
-				return true;
+				if (value === this.cells[i].element && this.cells[i].content === TYPE.EMPTY) {
+					return true;
+				}
 			}
-		}
-
-		//check columns
-		for (var i = 0; i <= 2; i++) {
-			if (board[i].content !== "E" && board[i].content === board[i + 3].content && board[i + 3].content === board[i + 6].content) {
-				this.result = board[i].content + "-won"; //update the state result
-				return true;
-			}
-		}
-
-		//check diagonals
-		for (var i = 0, j = 4; i <= 2; i = i + 2, j = j - 2) {
-			if (board[i].content !== "E" && board[i].content == board[i + j].content && board[i + j].content === board[i + 2 * j].content) {
-				this.result = board[i].content + "-won"; //update the state result
-				return true;
-			}
-		}
-
-		var emptyCellsAvailable = this.emptyCellsLeftOnBoard();
-
-
-		if (emptyCellsAvailable.length == 0) {
-			//the game is draw
-			this.result = "draw"; //update the state result
-			return true;
-		}
-		else {
 			return false;
-		}
+		};
+
+		Board.prototype.advanceTurn = function () {
+			if (this.turn === TYPE.NOUGHT) {
+				this.turn = TYPE.CROSS;
+			}
+			else {
+				this.turn = TYPE.NOUGHT;
+			}
+		};
+		Board.prototype.displayTurn = function () {
+			if (this.turn === TYPE.NOUGHT) {
+				$("#player2").removeClass('active');
+				$("#player1").addClass('active');
+			}
+			else if (this.turn === TYPE.CROSS) {
+				$("#player1").removeClass('active');
+				$("#player2").addClass('active');
+			}
+		};
+		/*
+		 This method is used to empty all of the cells on the board.
+		 */
+		Board.prototype.emptyAllCells = function () {
+			for (var i = 0; i < this.cells.length; i++) {
+				this.cells[i].clear();
+				$(this.items[i]).removeClass("box-filled-1 box-filled-2");
+			}
+		};
+		/*
+		 This method used to determine the amount of empty cells left.
+		 */
+		Board.prototype.emptyCellsLeftOnBoard = function () {
+			var indxs = [];
+			for (var itr = 0; itr < 9; itr++) {
+				if (this.cells[itr].content === TYPE.EMPTY) {
+					indxs.push(itr);
+				}
+			}
+			return indxs;
+		};
+
+		/*
+		 This method is used to determine if the game is finished, who won, and if there is a draw or not.
+		 This methods code used & slightly modified from https://mostafa-samir.github.io/Tic-Tac-Toe-AI/
+		 */
+		Board.prototype.isTerminal = function () {
+			var board = this.cells;
+			//check rows
+			for (var i = 0; i <= 6; i = i + 3) {
+				if (board[i].content !== TYPE.EMPTY && board[i].content === board[i + 1].content && board[i + 1].content === board[i + 2].content) {
+					this.result = board[i].content + "-won"; //update the state result
+					return true;
+				}
+			}
+
+			//check columns
+			for (var i = 0; i <= 2; i++) {
+				if (board[i].content !== TYPE.EMPTY && board[i].content === board[i + 3].content && board[i + 3].content === board[i + 6].content) {
+					this.result = board[i].content + "-won"; //update the state result
+					return true;
+				}
+			}
+
+			//check diagonals
+			for (var i = 0, j = 4; i <= 2; i = i + 2, j = j - 2) {
+				if (board[i].content !== TYPE.EMPTY && board[i].content === board[i + j].content && board[i + j].content === board[i + 2 * j].content) {
+					this.result = board[i].content + "-won"; //update the state result
+					return true;
+				}
+			}
+
+			var emptyCellsAvailable = this.emptyCellsLeftOnBoard();
 
 
-	};
+			if (emptyCellsAvailable.length === 0) {
+				//the game is draw
+				this.result = "draw"; //update the state result
+				return true;
+			}
+			else {
+				return false;
+			}
 
 
-	//var Board = require('./board');
+		};
+	}());
+
+	/*
+	Once page loads invoke main function with jQuery.
+	 */
 	jQuery(function ($) {
-
 		var board = new Board(),
-		winMessageContainer = $('#finish');
+			winMessageContainer = $('#finish');
 
 		$(".box").click(function (event) {
 			event.preventDefault();
@@ -214,6 +226,7 @@
 				board.displayTurn();
 			}
 		});
+
 		$('#finish a').click(function () {
 			//empty all the cells on the board.
 			board.emptyAllCells();
@@ -228,7 +241,6 @@
 		});
 
 		$('.box').hover(function () {
-
 			//Making sure cell hovered over is an empty cell.
 			if (board.isEmptyCell(this)) {
 				if (board.turn === TYPE.NOUGHT) {
@@ -238,46 +250,7 @@
 					$(this).toggleClass("box-hover2");
 				}
 			}
-
 		});
-
-		/*
-		 The finding variables code?
-		 */
-		function getGlobals() {
-			console.log("is it calling the getGlobasl?");
-			var differences = {},
-				exceptions,
-				globals = {},
-				ignoreList = (prompt('Ignore filter (comma sep)?', '') || '').split(','),
-				i = ignoreList.length,
-				iframe = document.createElement('iframe');
-			while (i--) {
-				globals[ignoreList[i]] = 1
-			}
-			for (i in window) {
-				differences[i] = {
-					'type': typeof window[i],
-					'val': window[i]
-				}
-			}
-			iframe.style.display = 'none';
-			document.body.appendChild(iframe);
-			iframe.src = 'about:blank';
-			iframe = iframe.contentWindow || iframe.contentDocument;
-			for (i in differences) {
-				if (typeof iframe[i] != 'undefined') delete differences[i];
-				else if (globals[differences[i].type]) delete differences[i]
-			}
-			exceptions = 'addEventListener,document,location,navigator,window'.split(',');
-			i = exceptions.length;
-			while (--i) {
-				delete differences[exceptions[i]]
-			}
-			console.dir(differences);
-		}
-
-		getGlobals();
 	});
 }();
 
